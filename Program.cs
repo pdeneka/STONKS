c32355ea9484ffed99225827d69e3f66afb80a00
Phillip Deneka <15189597+pdeneka@users.noreply.github.com>
2021-02-16 13:29:09 -0500

    moving things around

--- Program.cs
+++ Program.cs
@@ -10,89 +10,51 @@ namespace RegShoFinraDailyShorts
     {
         static void Main(string[] args)
         {
-            /***************************************************************
-             * 
-             *      FINRA Short File Data
-             * 
-             ****************************************************************/
-            string website = new string("http://regsho.finra.org/");
-            string filenamePart1 = new string("CNMSshvol");
-            //FP2 is the date in YYYYMMDD format
-            string filenamePart3 = new string(".txt");
-            DateTime StartDate = DateTime.Now;
+            Menu();
+        }
 
-            /***************************************************************
-             * 
-             *      Local Directory Data
-             * 
-             ****************************************************************/
-
-            string localDirectoryRoot = @"C:\Your\Path\Here\";
-
-            /***************************************************************
-             * 
-             *      Concatenated String
-             * 
-             ****************************************************************/
-
-            string url = new string("");
-            string destination = new string("");
-
-            /***************************************************************
-             * 
-             *      404 Error Handling
-             *      
-             *      The fail count needs to occur for ~two weeks straight before it stops.
-             *      Stock Exchange is closed on weekends (see above) and holidays
-             * 
-             ****************************************************************/
-
-            bool ResultsNot404 = true;
-            int ResultsFailCount = 0;
-
-            while (ResultsNot404)
+        public static void Menu() 
+        {
+            while (true)
             {
-                url = website + filenamePart1 + StartDate.ToString("yyyyMMdd") + filenamePart3;
-                destination = localDirectoryRoot + filenamePart1 + StartDate.ToString("yyyyMMdd") + filenamePart3;
+                Console.Clear();
+                Console.WriteLine("Please enter the # value of your selction.");
+                Console.WriteLine("\t1. Download Failure To Deliver aggregate data.");
+                Console.WriteLine("\t2. Download Daily Shorts aggregate data.");
+                Console.WriteLine("\tX. Exit.");
+                Console.WriteLine("\n");
+                /*
+                Console.WriteLine("3. ");
+                Console.WriteLine("4. ");
+                Console.WriteLine("5. ");
+                Console.WriteLine("6. ");
+                Console.WriteLine("7. ");
+                Console.WriteLine("8. ");
+                Console.WriteLine("9. ");
+                */
 
-                if ((int)StartDate.DayOfWeek > 0 && (int)StartDate.DayOfWeek < 6) // Sunday & Saturday respectively
-                {
-                    Console.WriteLine("Pulling down file: {0}", url);
 
-                    using (WebClient client = new WebClient())
-                    {
-                        try
-                        {
-                            client.DownloadFile(url, destination);
-                            Console.WriteLine("\tSuccess! Downloading file {0}", url);
-                            ResultsFailCount = 0;
-                        }
-                        catch (WebException wex)
-                        {
-                            if (((HttpWebResponse)wex.Response).StatusCode == HttpStatusCode.NotFound)
-                            {
-                                Console.WriteLine("Unable to download {0}.  404 Not found.", url);
+                switch (Console.ReadLine()) { 
+                    case "1":
+                        SECFTD FTDs = new SECFTD(DateTime.Now, new DateTime(1970, 01, 01), @"D:\Stocks\SEC\FTD");
+                        FTDs.Pull();
+                        break;
 
-                                ResultsFailCount++;
-                                if (ResultsFailCount >= 10)
-                                {
-                                    ResultsNot404 = false;
-                                }
-                            }
-                        }
-                    }
+                    case "2":
+                        RegShoFinraDailyShorts DailyShorts = new RegShoFinraDailyShorts(DateTime.Now, new DateTime(1970, 01, 01), @"D:\Stocks\RegShoFinra\DailyShorts");
+                        DailyShorts.Pull();
+                        break;
+
+                    case "3": break;
+                    case "X": 
+                    case "x":
+                        Environment.Exit(0);
+                        break;
                 }
-                else
-                {
-                    Console.WriteLine("Date {0} falls on weekend.", StartDate.ToString("yyyyMMdd"));
-                }
-                StartDate = StartDate.AddDays(-1);
 
-                //Do not abuse those that give us the snackies.
-                Thread.Sleep(50);
+
             }
-
-            Console.WriteLine("Loop ended. {0} failures occurred ending on {1}", ResultsFailCount, StartDate);
+        
         }
     }
 }
